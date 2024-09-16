d8c15e3a5938ccb48d0ae4cf84135e47185b178e
William Okano <williamokano@gmail.com>
2024-09-17 00:51:26 +0200

    feat: added missed tests

--- src/test/kotlin/dev/okano/kotlintesting/helpers/MutableClockTest.kt
+++ src/test/kotlin/dev/okano/kotlintesting/helpers/MutableClockTest.kt
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test
 import java.time.Clock
 import java.time.Duration
 import java.time.Instant
+import java.time.temporal.ChronoUnit
 
 class MutableClockTest {
 
@@ -30,7 +31,7 @@ class MutableClockTest {
     }
 
     @Test
-    fun `should be able to advance time`() {
+    fun `should be able to advance time with duration`() {
         val clock = MutableClock(Instant.parse("2024-09-19T14:00:00Z"))
         clock.advance(Duration.ofMinutes(15))
 
@@ -38,9 +39,25 @@ class MutableClockTest {
     }
 
     @Test
-    fun `should be able to rewind time`() {
+    fun `should be able to advance time with temporal unit`() {
+        val clock = MutableClock(Instant.parse("2024-09-19T14:00:00Z"))
+        clock.advance(15, ChronoUnit.MINUTES)
+
+        clock.instant() shouldBeEqual Instant.parse("2024-09-19T14:15:00Z")
+    }
+
+    @Test
+    fun `should be able to rewind time with duration`() {
         val clock = MutableClock(Instant.parse("2024-09-19T14:15:00Z"))
         clock.rewind(Duration.ofMinutes(13))
+
+        clock.instant() shouldBeEqual Instant.parse("2024-09-19T14:02:00Z")
+    }
+
+    @Test
+    fun `should be able to rewind time with temporal unit`() {
+        val clock = MutableClock(Instant.parse("2024-09-19T14:15:00Z"))
+        clock.rewind(13, ChronoUnit.MINUTES)
 
         clock.instant() shouldBeEqual Instant.parse("2024-09-19T14:02:00Z")
     }
